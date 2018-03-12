3d28bf68752cbb2df0a5ba4a501d1fa78fdb0964
Jun Li <june11084@gmail.com>
2018-03-11 20:10:15 -0700

    create template

---
+++ app/src/main/java/com/example/group/myrestaurant/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.group.myrestaurant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/group/myrestaurant/RestaurantActivity.java
@@ -0,0 +1,18 @@
+package com.example.group.myrestaurant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RestaurantActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
+    }
+}
